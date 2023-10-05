9d27edf857b3210a550dff3ad64caaf829ad9607
Brynn Brancamp <brynnbrancamp@gmail.com>
2023-10-05 18:28:21 -0500

    Login screen wip.

--- src/routes/+layout.server.js
+++
@@ -1,6 +0,0 @@
-
-export const prerender = true;
-
-export const ssr = true;
-
-export const trailingSlash = 'ignore';
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../../app.postcss";
+    import "./login.postcss";
+</script>
+
+<div id = "container">
+    <div id = "login">
+        <div id = "logo">
+            <img class = "picture" src = "logo.png"/>
+        </div>
+        <div id = "form">
+            <input id = "user" class = "input" placeholder="Email or Username"/>
+            <input id = "submit" class = "login input" type="submit" value="Login"/>
+            <input id = "submit" class = "signup input" type="submit" value="Signup"/>
+        </div>
+    </div>
+</div>
